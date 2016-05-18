b2807a23550b5d926980199cfa436bd1f61da157
Christian Davatz <crixx@davatz.eu>
2016-05-18 13:08:47 +0200

    fixed sting expansion

--- jm-acmeair-default-assets/recipes/default.rb
+++ jm-acmeair-default-assets/recipes/default.rb
@@ -4,7 +4,7 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-jmeter_root_dir = node['acmeairdefaultassets']['jmeter']['root_dir']
+jmeter_root_dir = node[:acmeairdefaultassets][:jmeter][:root_dir]
 
 #insert the driver.jar
 cookbook_file "#{jmeter_root_dir}/lib/ext/acmeair-jmeter-1.1.0-SNAPSHOT.jar" do
@@ -14,19 +14,19 @@ cookbook_file "#{jmeter_root_dir}/lib/ext/acmeair-jmeter-1.1.0-SNAPSHOT.jar" do
 end
 
 #insert the json-mapper.jar
-cookbook_file '#{jmeter_root_dir}/lib/ext/json-simple-1.1.1.jar' do
+cookbook_file "#{jmeter_root_dir}/lib/ext/json-simple-1.1.1.jar" do
   source 'json-simple-1.1.1.jar'
   mode '0644'
   action :create
 end
 
-cookbook_file '#{jmeter_root_dir}/bin/Airports.csv' do
+cookbook_file "#{jmeter_root_dir}/bin/Airports.csv" do
   source 'Airports.csv'
   mode '0644'
   action :create
 end
 
-cookbook_file '#{jmeter_root_dir}/bin/Airports2.csv' do
+cookbook_file "#{jmeter_root_dir}/bin/Airports2.csv" do
   source 'Airports2.csv'
   mode '0644'
   action :create
